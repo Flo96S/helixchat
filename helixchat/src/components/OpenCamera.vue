9277b2de54754011abde455f4b69172fea15c766
xxelectron <me@florian-s.de>
2024-04-09 10:41:49 +0200

    first commit

---
+++ helixchat/src/components/OpenCamera.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <button @click="() => {
+      console.log('Open camera')
+    }">Open camera</button>
+  </div>
+</template>
---
+++ helixchat/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import OpenCamera from '../components/OpenCamera.vue';
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <OpenCamera />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
