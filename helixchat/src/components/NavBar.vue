0a095d78ffaf8a5aacb1dadc781402f8f65dd741
xxelectron <me@florian-s.de>
2024-04-16 16:35:20 +0200

    Refactor CSS styles in multiple components and update NavBar and HomeView components

--- helixchat/src/components/NavBar.vue
+++ helixchat/src/components/NavBar.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 import "../assets/tailwind.css";
+import router from '@/router';
+
+function ToProfile() {
+   console.log("To profile");
+   router.push("/profile")
+}
 </script>
 
 <template>
-<<<<<<< HEAD
-   <div class="w-100 max-w-screen-xl bg-red flex">
-      <a>Nav</a>
-      <img src="../assets/logo.webp" class="w-4 h-4 flex" />
-=======
-   <div class="navbar">
-      <a class="">Nav</a>
-      <div>
-         Hello
+   <div class="max-w-screen-xl bg-gradient-to-t from-slate-900 to-slate-800 flex h-16">
+      <img src="../assets/logo.webp" class="m-2" />
+      <div class="flex items-center h-full">
+         <span class="text-2xl font-semibold text-white">HelixChat</span>
       </div>
->>>>>>> 70d80e83173fce76194c6db1d931440d5c5281c5
+      <div @click="ToProfile" class="flex items-center cursor-pointer float-end mr-4 ml-auto">
+         <a class="text-white"><img class="mr-0 h-8 w-8" src="../assets/profilefill.svg" /></a>
+      </div>
    </div>
 </template>
 
